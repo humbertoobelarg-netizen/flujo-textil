fe4a0cf985e4d5a766412c1e82421863b2126c5e
humbertoobelarg-netizen <humbertoobelarg@gmail.com>
2026-06-29 16:16:13 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('App lista para instalar'))
+      .catch(err => console.log('Error al registrar app', err));
+  });
+}
